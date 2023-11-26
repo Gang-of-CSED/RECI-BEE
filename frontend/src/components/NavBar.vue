0ae5a4642e380003a1ccb60dd9bb527cd71142e2
AbdElRahman Osama <bodyusama207@gmail.com>
2023-11-27 00:57:24 +0200

    Adding NavBar 1.0

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,15 +1,55 @@
 <template>
     <div class="navbar">
-        Navbar
+        <div><h1>RECI-BEE</h1></div>
+        <ul>
+            <li><a href="#">Browse Catalogue</a></li>
+            <li><a href="#">Dish Of The Day</a></li>
+            <li><a href="#">Random Dish</a></li>
+            <li><a href="#">About Us</a></li>
+            <li><h3>AbdElRahman Osama</h3></li>
+        </ul>
     </div>
 </template>
-  
-<script>
 
+<script>
+export default {
+    name: 'NavBar',
+}
 </script>
-  
+
 <style scoped>
-    .navbar {
-        border: 3px solid rgb(164, 32, 155);
-    }
+.navbar {
+    background-color: #FBF7EB;
+    height: 116px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: #312525;
+}
+h1 {
+    margin-left: 57px;
+    font-size: 36px;
+}
+h3 {
+    margin-left: 19px;
+    font-size: 20px;
+}
+ul {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    list-style: none;
+    margin: 0 25.5px 0 0;
+    padding: 0;
+}
+li {
+    margin: 0 20.5px;
+    font-size: 15px;
+}
+a {
+    text-decoration: none;
+    color : #312525;
+}
+
+
 </style>
