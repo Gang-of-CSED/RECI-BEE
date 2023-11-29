de21a6515b97d92d4ed8f8c374e4101ac6900b73
DEIf <you@example.com>
2023-11-29 22:32:54 +0200

    push recipeInfo section

--- frontend/src/views/RecipeDetail/components/RecipeInfo.vue
+++ frontend/src/views/RecipeDetail/components/RecipeInfo.vue
@@ -1,16 +1,107 @@
 <template>
     <div class="recipe-info">
-        RecipeInfo
+        <div class="recipeImg">
+            <img class="Img" :src="imageSrc" alt="recipe" />
+        </div>
+        <div class="Ingredients">
+            <div class="intText">Ingredients:</div>
+            <div class="t1" v-html="ingredientsContent"></div>
+        </div>
+        <div class="breakLine">
+            <hr>
+        </div>
+        <div class="Steps">
+            <div class="stepText">Steps:</div>
+            <div class="t2" v-html="stepsContent"></div>
+        </div>
     </div>
 </template>
 
-<script setup>
+<script>
+    export default {
+    data() {
+        return {
+        imageSrc:require('@/assets/image2.png'),
+        ingredientsContent:'',
+        stepsContent:''
+        };
+    },
+    mounted() {
+    import('./tempData.js').then((module) => {
+      this.ingredientsContent = module.ingContent;
+      this.stepsContent = module.stepContent
+    });
+    },
+    methods: {
+        
+    },
+    };
 </script>
 
 <style scoped>
 .recipe-info {
     width: 50%;
-    border: 3px solid rgb(0, 107, 128);
     height: 100%;
+    border: 3px solid rgb(0, 107, 128);
+    background: #FBF7EB;
+;
+}
+.recipeImg{
+    width: 90%;
+    height: 362px;
+    border: 1px #FBF7EB solid;
+    border-radius: 15px;
+    margin-top: 50px;
+    margin-left: 5%;
+
+}
+.Img{
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+}
+
+.Ingredients{
+    width: 90%;
+    height: auto;
+    border: #FBF7EB 1px solid;
+    margin-top: 37px;
+    margin-left: 5%;
+
+}
+
+.breakLine{
+    width: 90%;
+    height: 1px;
+    border:#312525 1px solid;
+    margin-top: 46px;
+    margin-left: 5%;
+}
+
+.Steps{
+    width: 90%;
+    
+    border: #FBF7EB 1px solid;
+    margin-top: 37px;
+    margin-left: 5%;
+    margin-bottom: 54px;
+}
+
+.intText,.stepText{
+    color: #312525;
+    font-size: 18px;
+    line-height: 19.36px;
+    font-weight: 700;
+    margin-bottom: 22px;
+}
+
+.t1,.t2{
+    color: #312525;
+    font-size: 14px;
+    line-height: 19px;
+    letter-spacing: 0.098em;
+    text-align: left;
+    font-weight: 400;
+
 }
 </style>
---
+++ frontend/src/views/RecipeDetail/components/tempData.js
@@ -0,0 +1,21 @@
+export const ingContent = '<b>For The Meat Sauce:</b>'+
+'<br>Ground beef - Onion - Tomato sauce (Canned/Crushed tomatoes) - Olive Oil - Thyme - Salt - Black pepper'+
+'<br><br><b>For The Béchamel Sauce:</b>'+
+'<br>Flour - Unsalted butter - Milk - Salt - Nutmeg'+
+'<br><br><b>For The pasta:</b>'+
+'<br>Penne pasta - Water - Salt';
+
+export const stepContent = 'Making an Egyptian pasta bake, like Béchamel pasta, '+
+'is easier than you’d think! The process is actually very similar to making a lasagna – except, '+
+'you’re using penne pasta instead of lasagna sheets, '+
+'and a creamy béchamel sauce instead of the traditional tomato-based sauce you see in many lasagnas. Here are the main steps:'+
+'<br><br><b>Prepare The Pasta</b>'+
+'<br>Nothing special here! You’re just preparing the pasta as you always would.'+
+'<br><br><b>Make The Béchamel Mixture</b>'+
+'<br>Luckily, Béchamel is one of the easiest mother sauces to prepare. '+
+'It’s simply a matter of mixing melted butter with flour and milk over a low heat.'+
+'<br><br><b>Macarona Bechamel, Assemble!</b>'+
+'<br>In a large baking dish, Add the components you’ve already made: '+
+'pasta first, then stir in the meat sauce. Next is a layer of mozzarella cheese, '+
+'and the finally layer is a generous helping of the creamy béchamel sauce you’ve made. '+
+'Smooth it out, bake, and then you’re ready for an Egyptian feast!';
