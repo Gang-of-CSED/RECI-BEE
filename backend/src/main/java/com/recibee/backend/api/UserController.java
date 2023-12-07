f5dad966a662e69131913fc1e6117868a8afcb72
Ahmad Hassan <mi010203269@gmail.com>
2023-12-07 18:38:41 +0200

    add authenticatoin with jwt

--- backend/src/main/java/com/recibee/backend/api/UserController.java
+++ backend/src/main/java/com/recibee/backend/api/UserController.java
@@ -1,31 +1,53 @@
 package com.recibee.backend.api;
 
+import java.time.temporal.ChronoUnit;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
 import com.recibee.backend.managers.UserManager;
 import com.recibee.backend.models.UserModel;
+import io.jsonwebtoken.Jwts;
+import java.util.Date;
+import com.recibee.backend.config.Auth;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @CrossOrigin(origins = { "http://localhost:8081" })
 public class UserController {
-    
+
     @PostMapping("/login")
-    public UserModel login(String username, String password) {
+    public String login(String username, String password) {
         UserModel user = UserManager.getInstance().getUser(username);
         if (user != null && user.getPassword().equals(password)) {
-            return user;
+            // generate jwt from username
+            String jwtToken = Auth.getToken(username);
+
+            return jwtToken;
         }
         return null;
     }
 
     @PostMapping("/register")
     public UserModel register(String username, String password, String name) {
+        if (username == null || password == null || name == null)
+            return null;
         UserModel user = new UserModel(username, password, name);
         UserManager.getInstance().addUser(user);
         return user;
     }
 
+    @GetMapping("/test")
+    public UserModel test(@RequestHeader String authorization) {
+        try {
+            return Auth.getUserModel(authorization);
 
-    
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
 }
---
+++ backend/src/main/java/com/recibee/backend/config/Auth.java
@@ -0,0 +1,46 @@
+package com.recibee.backend.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import javax.crypto.spec.SecretKeySpec;
+
+import com.recibee.backend.managers.UserManager;
+import com.recibee.backend.models.UserModel;
+
+import java.security.Key;
+import java.util.Base64;
+
+public class Auth {
+
+    static String secret = "asdfSFS34wfsdfsdfSDSD32dfsddDDerQSNCK34SOWEK5354fdgdf4";
+
+    static Key hmacKey = new SecretKeySpec(Base64.getDecoder().decode(secret),
+            SignatureAlgorithm.HS256.getJcaName());
+
+    public static String getToken(String username) {
+        return Jwts.builder()
+                .setId(username)
+                .setSubject("account")
+                .signWith(hmacKey)
+                .compact();
+    }
+
+    public static String getUser(String jwtString) {
+
+        Jws<Claims> jwt = Jwts.parserBuilder()
+                .setSigningKey(hmacKey)
+                .build()
+                .parseClaimsJws(jwtString);
+
+        return jwt.getBody().getId();
+    }
+
+    public static UserModel getUserModel(String jwtString) {
+
+
+        String username = getUser(jwtString);
+        return UserManager.getInstance().getUser(username);
+    }
+}
