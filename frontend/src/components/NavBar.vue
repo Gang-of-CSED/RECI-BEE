94afdba853e5fbd4fea2b4c3f285e2d1f60c7b69
Ab00dMD <abdalrhmanmohamed94@gmail.com>
2023-12-09 06:17:28 +0200

    Login&SignUp

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -9,8 +9,32 @@
             <li>
                 <h3 v-if="logged">{{ username }}</h3>
                 <div v-else class="log">
-                    <button class="login">LOG IN</button>
-                    <button class="signup">SIGN UP</button>
+                    <button class="login" onclick="document.getElementById('Login').style.display='block'">LOG IN</button>
+                        <div id="Login" class="formC">
+                        <form class="formC-content animate" action="/login" method="post">
+                            <div class="container">
+                            <p>sign In</p>
+                            <input class="text" type="text" placeholder="username/email" name="username" required>
+                            <input class="psw" type="password" placeholder="password" name="psw" required>
+                            <button class="sbt" type="submit">Enter</button>
+                            </div>
+                        </form>
+                        </div>
+
+                        <button class="signup" onclick="document.getElementById('Signup').style.display='block'">SIGN UP</button>
+                        <div id="Signup" class="formC">
+                        <form class="formC-content2 animate" action="/signup" method="post">
+                            <div class="container">
+                            <p>sign Up</p>
+                            <input class="text" type="text" placeholder="username" name="username" required>
+                            <input class="text" type="text" placeholder="email" name="email" required>
+                            <input class="psw" type="password" placeholder="password" name="psw" required>
+                            <input class="psw" type="password" placeholder="confirm password" name="cpsw" required>
+                            <button class="sbt" type="submit">Enter</button>
+                            </div>
+                        </form>
+                        </div>
+
                 </div>
             </li>
         </ul>
@@ -25,7 +49,20 @@ export default {
             username:"Abdelrhman Deif",
             logged: false,
         }
-    }
+    },
+    mounted(){
+        var formC = document.getElementById('Login');
+        var formC2= document.getElementById('Signup');
+        window.onclick = function(event) {
+            if (event.target == formC) {
+                formC.style.display = "none";
+            }
+            if (event.target == formC2) {
+                formC2.style.display = "none";
+            }
+
+        }
+    },
 }
 </script>
 
@@ -149,5 +186,101 @@ a {
         font-size: 1.25vw;
     }
 }
+
+input[type=text], input[type=password] {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    box-sizing: border-box;
+}
+
+.sbt{
+    background-color: #E35733;
+    color: #FBF7EB;
+    margin:12% 37%;
+    border-radius: 10px;
+    cursor: pointer;
+    width: 83px;
+    height: 42px;
+    font-size: 16px;
+    font-weight: 300;
+    line-height: 19px;
+    letter-spacing: 0em;
+    text-align: center;
+}
+
+.sbt:hover {
+    opacity: 0.8;
+}
+.sbt:active{
+    scale: 110%;
+}
+
+.container {
+    padding: 16px;
+}
+
+.container p{
+    margin: 8% auto ;
+    font-size: 48px;
+    font-weight: 700;
+    line-height: 58px;
+    letter-spacing: 0em;
+    text-align: center;
+    color: #FBF7EB;
+}
+
+.text, .psw{
+    width: 273px;
+    height: 64px;
+    border-radius: 10px;
+    box-shadow: 0 2px 0px rgba(0, 0, 0, 0.7);
+    background-color: #FBF7EB;
+}
+
+.formC {
+    display: none; 
+    position: fixed; 
+    z-index: 1; 
+    left: 0;
+    top: 0;
+    width: 100%; 
+    height: 100%; 
+    background-color: rgba(0,0,0,0.4); 
+    padding-top: 60px;
+}
+
+.formC-content, .formC-content2{
+    border-radius: 15px;
+}
+.formC-content{
+    margin: 5% auto 15% auto; 
+    background-color: #FBBC3B;
+    width: 360px; 
+    height: 400px;
+}
+.formC-content2{
+    margin: 3% auto 10% auto; 
+    background-color: #B3D0E8;
+    width: 360px; 
+    height: 569px;
+}
+
+.animate {
+  -webkit-animation: animatezoom 0.6s;
+  animation: animatezoom 0.6s
+}
+
+@-webkit-keyframes animatezoom {
+  from {-webkit-transform: scale(0)} 
+  to {-webkit-transform: scale(1)}
+}
+  
+@keyframes animatezoom {
+  from {transform: scale(0)} 
+  to {transform: scale(1)}
+}
+
 
 </style>
--- frontend/src/views/LandingPage/components/LandingPageDetails.vue
+++ frontend/src/views/LandingPage/components/LandingPageDetails.vue
@@ -48,14 +48,32 @@
                     Build custom collections,<br>never forget a cherished recipe. Your profile,<br>your culinary adventure.
                 </p>
                 <div class="btns1">
-                    <button class="login">LOG IN</button>
-                    <button class="signup">SIGN UP</button>
+                    <button class="login" onclick="document.getElementById('Login').style.display='block'">LOG IN</button>
+                        <div id="Login" class="formC">
+                        <form class="formC-content animate" action="/login" method="post">
+                            <div class="container">
+                            <p>sign In</p>
+                            <input class="text" type="text" placeholder="username/email" name="username" required>
+                            <input class="psw" type="password" placeholder="password" name="psw" required>
+                            <button class="sbt" type="submit">Enter</button>
+                            </div>
+                        </form>
+                        </div>
+                    <button class="signup" onclick="document.getElementById('Signup').style.display='block'">SIGN UP</button>
+                        <div id="Signup" class="formC">
+                        <form class="formC-content2 animate" action="/signup" method="post">
+                            <div class="container">
+                            <p>sign Up</p>
+                            <input class="text" type="text" placeholder="username" name="username" required>
+                            <input class="text" type="text" placeholder="email" name="email" required>
+                            <input class="psw" type="password" placeholder="password" name="psw" required>
+                            <input class="psw" type="password" placeholder="confirm password" name="cpsw" required>
+                            <button class="sbt" type="submit">Enter</button>
+                            </div>
+                        </form>
+                        </div>
                 </div>
-
             </div>
-        </div>
-        <div class="footer">
-            
         </div>
     </div>
 </template>
@@ -73,6 +91,18 @@
         };
     },
     mounted() {
+        var formC = document.getElementById('Login');
+        var formC2= document.getElementById('Signup');
+        window.onclick = function(event) {
+            if (event.target == formC) {
+                formC.style.display = "none";
+            }
+            if (event.target == formC2) {
+                formC2.style.display = "none";
+            }
+
+        }
+
     },
     methods: {
         
@@ -250,7 +280,7 @@
     padding-left: 5%;
     padding-right: 5%;
     position: relative;
-    z-index: 12;
+    z-index: 5;
 }
 .content1 .p1{
     font-size: 48px;
@@ -261,7 +291,6 @@
     margin-top: 5px;
     margin-bottom: 15px;
     color: #FBF7EB;
-;
 }
 .content1 .p2{
     font-size: 16px;
@@ -313,6 +342,100 @@
 }
 
 
+input[type=text], input[type=password] {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    box-sizing: border-box;
+}
+
+.sbt{
+    background-color: #E35733;
+    color: #FBF7EB;
+    margin:12% 37%;
+    border-radius: 10px;
+    cursor: pointer;
+    width: 83px;
+    height: 42px;
+    font-size: 16px;
+    font-weight: 300;
+    line-height: 19px;
+    letter-spacing: 0em;
+    text-align: center;
+}
+
+.sbt:hover {
+    opacity: 0.8;
+}
+.sbt:active{
+    scale: 110%;
+}
+
+.container {
+    padding: 16px;
+}
+
+.container p{
+    margin: 8% auto ;
+    font-size: 48px;
+    font-weight: 700;
+    line-height: 58px;
+    letter-spacing: 0em;
+    text-align: center;
+    color: #FBF7EB;
+}
+
+.text, .psw{
+    width: 273px;
+    height: 64px;
+    border-radius: 10px;
+    box-shadow: 0 2px 0px rgba(0, 0, 0, 0.7);
+    background-color: #FBF7EB;
+}
+
+.formC {
+    display: none; 
+    position: fixed; 
+    z-index: 1; 
+    left: 0;
+    top: 0;
+    width: 100%; 
+    height: 100%; 
+    background-color: rgba(0,0,0,0.4); 
+    padding-top: 60px;
+}
+
+.formC-content, .formC-content2{
+    border-radius: 15px;
+}
+.formC-content{
+    margin: 5% auto 15% auto; 
+    background-color: #FBBC3B;
+    width: 360px; 
+    height: 400px;
+}
+.formC-content2{
+    margin: 3% auto 10% auto; 
+    background-color: #B3D0E8;
+    width: 360px; 
+    height: 569px;
+}
+
+.animate {
+  -webkit-animation: animatezoom 0.6s;
+  animation: animatezoom 0.6s
+}
+
+@-webkit-keyframes animatezoom {
+  from {-webkit-transform: scale(0)} 
+  to {-webkit-transform: scale(1)}
+}
+  
+@keyframes animatezoom {
+  from {transform: scale(0)} 
+  to {transform: scale(1)}
+}
 
 
 
