0c27bb4469705bb0da42f051b9a83874614efa45
Ab00dMD <abdalrhmanmohamed94@gmail.com>
2023-12-30 02:09:09 +0200

    Adding recipe pagination

--- frontend/src/views/RecipeList/components/List.vue
+++ frontend/src/views/RecipeList/components/List.vue
@@ -1,12 +1,29 @@
 <template>
    <div class="list">
-
      <!-- <div class="slogan"><h6>Unlock The<br>Flavors Of The World</h6></div> -->
 
      <div class="recipies">
-         <Card v-for="card in recipiesArray" :key="card.name" :card-data="card" @toggle-favorite="toggleFavorite" class="cards" :user="user" />
+         <Card v-for="card in recipiesSubArray" :key="card.name" :card-data="card" @toggle-favorite="toggleFavorite" class="cards" :user="user" />
      </div>
+     <div class="pagin-bar">
+        <button class="pagin-btn" @click="getPre"> &lt; </button>
+        <span v-for="(item, index) in new Array(pageNums)" :key="index">
+            <button class="pagin-btn" @click="goToPage(index+1)" :style="{ backgroundColor: index + 1 === currentPage ? '#FBBC3B' : '#E35733' }">{{ index+1 }}</button>
+        </span>
+        <button class="pagin-btn" @click="getNext"> &gt; </button>
+        <span> recipes in one page:
+          <select id="inPage" name="choices" v-model="nums" @change="changeNums">
+              <option value="8">8</option>
+              <option value="16">16</option>
+              <option value="32">32</option>
+              <option value="64">64</option>
+              <option value="128">128</option>
+          </select>
+          </span>
 
+    </div>
+
+     
    </div>
 </template>
 
@@ -18,7 +35,7 @@ import axios from 'axios';
 export default {
   name: 'List.vue',
   components: {
-    Card, 
+    Card,
   },
   props: {
     recipiesArray:Array,
@@ -27,6 +44,12 @@ export default {
   data() {
     return {
       // recipiesArray: [],
+      recipiesSubArray: [],
+      pageNums: 0,
+      currentPage: 1,
+      nums: 8,
+      start: 0, 
+      end: 10,
     };
   },
   methods: {
@@ -55,16 +78,48 @@ export default {
                 console.error('There was an error!', error);
               });
     },
+    getNext() {
+      if(this.end >= this.recipiesArray.length){return;}
+      this.start = this.end;
+      this.end = Math.min(this.end + this.nums, this.recipiesArray.length);
+      this.currentPage += 1;
+      this.updateSubArray();
+    },
+    getPre() {
+      this.start = Math.max(0, this.start - this.nums);
+      this.end = Math.min(this.start + this.nums, this.recipiesArray.length);
+      if(this.currentPage > 1)this.currentPage -= 1;
+      this.updateSubArray();
+    },
+    updateSubArray() {
+      this.recipiesSubArray = this.recipiesArray.slice(this.start, this.end);
+      this.pageNums = this.calcPageNums();
+    },
+    goToPage(p){
+      this.currentPage = p;
+      this.start = Math.min((p - 1) * this.nums, this.recipiesArray.length);
+      this.end = Math.min(this.start + this.nums, this.recipiesArray.length);
+      this.updateSubArray();
+    },
+    calcPageNums(){
+      return  this.pageNums = Math.ceil(this.recipiesArray.length / this.nums);
+    },
+    changeNums(){
+        this.goToPage(1);
+        this.nums = parseInt(this.nums);
+        console.log(this.nums);
+    }
+   
 
   },
-  // mounted(){
+  mounted(){
   //   // fetch('http://localhost:3000/recipiesArray')
   //   // .then(response => response.json()) // Convert the response to JSON
   //   // .then(data => {
   //   //   this.recipiesArray = data;
   //   // })
   //   // .catch(err => console.error("Error fetching recipes:", err));
-  // }
+  },
 };
 </script>
 <style scoped>
@@ -90,6 +145,24 @@ export default {
   /* flex: 0 0 225;  */
   box-sizing: border-box; 
  
+}
+.pagin-btn{
+    padding: 8px;
+    margin: 2px;
+    border-radius: 20px;
+    font-size: 1em;
+    cursor: pointer;
+    background-color: #E35733;
+    color: #FBF7EB;
+    width: auto;
+}
+.pagin-bar{
+  margin-bottom: 10%;
+}
+#inPage{
+  width: auto;
+  height: auto;
+  margin-left: 10px;
 }
 /* .slogan{
   
